98439e9e686e2ad03e090455a74569d3445b2b6e
hvKalista <alonzo.sabino814@gmail.com>
2023-07-10 11:38:56 -0500

    fixed error

--- src/utils/database/bot-error-db-utils.ts
+++ src/utils/database/bot-error-db-utils.ts
@@ -6,6 +6,12 @@ type BotErrorOptions = Database['public']['Tables']['bot_errors']['Insert'];
 
 export class BotErrorDbUtils {
     public static async createError(options: BotErrorOptions): Promise<void> {
+        const guildDoc = await supabase.from('guilds').select('id').eq('id', options.guild_id);
+        if (!guildDoc) options.guild_id = null;
+        const userDoc = await supabase.from('users').select('id').eq('id', options.user_id);
+        if (!userDoc) options.user_id = null;
+        const newsDoc = await supabase.from('news').select('id').eq('id', options.news_id);
+        if (!newsDoc) options.news_id = null;
         const { error } = await supabase.from('bot_errors').insert(options);
         if (error) throw new Error(`Could not create bot error in database:\n${error.message}`);
     }
