37a801cb9ca53f571fde3305377ddc1cb6504563
hvKalista <alonzo.sabino814@gmail.com>
2023-07-10 13:31:26 -0500

    interaction fixes

--- src/services/event-data-service.ts
+++ src/services/event-data-service.ts
@@ -35,8 +35,7 @@ export class EventDataService {
                 ? options.guild.preferredLocale
                 : Language.Default;
         if (options.user) {
-            let userData = await UserDbUtils.getUserById(options.user.id);
-            if (!userData) userData = await UserDbUtils.createUser(options.user);
+            const userData = await UserDbUtils.createUser(options.user);
             return new EventData(lang, langGuild, userData);
         }
         return new EventData(lang, langGuild);
