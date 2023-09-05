29761ed43d2bda9777d1a621ca29c746bd3b4219
hvKalista <alonzo.sabino814@gmail.com>
2023-09-05 15:47:40 -0500

    restart fix

--- src/models/bot.ts
+++ src/models/bot.ts
@@ -510,7 +510,9 @@ export class Bot {
     private async onNewsSent(newsId: number, shard: number): Promise<void> {
         try {
             const syndicateGuild = this.options.client.guilds.cache.get(config.syndicateGuildId);
-            if (!syndicateGuild) return;
+            if (!syndicateGuild) {
+                process.exit();
+            }
             const news = await NewsDbUtils.getNews(newsId);
             if (!news) {
                 return;
@@ -523,7 +525,7 @@ export class Bot {
             }
             await adminChannel.send(`Shard (${shard + 1}/${7}) finished sending news ${newsId}, now restarting...`);
 
-            process.exit(0);
+             process.exit();
         } catch (error) {
             await Logger.error({
                 message: `Failed to send news sent notification for news ${newsId}`,
