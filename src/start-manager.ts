d613c69ebd7095162a627a3721a3814e752c3955
hvKalista <alonzo.sabino814@gmail.com>
2023-10-04 16:48:13 -0500

    shard fix

--- src/start-manager.ts
+++ src/start-manager.ts
@@ -61,7 +61,7 @@ async function start(): Promise<void> {
     // }
 
     // get total guilds
-    let totalShards = 10;
+    let totalShards = 9;
 
     let shardList = MathUtils.range(0, totalShards);
 
